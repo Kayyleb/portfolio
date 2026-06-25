55c8e449e211131727312429b9f4197f75e7c3f3
Kayleb Cook <kaylebjuliancook@gmail.com>
2026-06-25 03:53:16 -0700

    cleaning up some code

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -3,18 +3,23 @@ import { projects } from "./projects";
 
 export default function Projects() {
   return (
-    <div className="grid gap-6">
+    <div className="mt-20">
 
-      {projects.map((project) => (
-        <ProjectCard
-          key={project.name}
-          name={project.name}
-          description={project.description}
-          skills={project.skills}
-          link={project.link}
-        />
-      ))}
+        <p className="text-2xl font-semibold tracking-tight text-forest text-center sm:text-3xl">
+            ALL PROJECTS
+        </p>
 
+        <div className="mt-8 grid grid-cols-1 gap-6 sm:grid-cols-3">
+            {projects.map((project) => (
+                <ProjectCard
+                key={project.name}
+                name={project.name}
+                description={project.description}
+                skills={project.skills}
+                link={project.link}
+                />
+            ))}
+        </div>
     </div>
   )
 }
